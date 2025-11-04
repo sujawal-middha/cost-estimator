993266549cc2ae6e4e8b3d169c4d88eabf28b6c1
sujawal-middha <143802238+sujawal-middha@users.noreply.github.com>
2025-11-04 14:12:25 +0530

    Revert "vercel reconfig"

--- estimator_UI/vite.config.js
+++ estimator_UI/vite.config.js
@@ -1,14 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    outDir: "dist", // <-- important: tells Vercel where the final files go
-  },
-  server: {
-    port: 5173,
-    open: true,
-  },
-});
+  plugins: [react(),  tailwindcss()],
+})
