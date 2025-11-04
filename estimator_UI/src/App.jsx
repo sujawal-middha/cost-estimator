a2792748a0c13e81f4cb5b50a6254cf919d6fe31
sujawal-middha <middha20@gmail.com>
2025-11-04 12:54:07 +0530

    adding vercel config chages and some ui changes

--- estimator_UI/src/App.jsx
+++ estimator_UI/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import FeatureSelection from "./pages/FeatureSelection";
@@ -7,7 +7,6 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 function App() {
-
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
@@ -20,7 +19,7 @@ function App() {
       </main>
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
