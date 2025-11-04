a25d4264d84578b5314f8e9a4d5b50691010e2cb
sujawal-middha <middha20@gmail.com>
2025-11-04 15:16:00 +0530

    changes

--- estimator_UI/vite.config.js
+++ estimator_UI/vite.config.js
@@ -2,13 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    outDir: "dist", // <-- important: tells Vercel where the final files go
-  },
-  server: {
-    port: 5173,
-    open: true,
-  },
 });
